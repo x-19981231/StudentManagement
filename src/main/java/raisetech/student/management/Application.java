f310962b5ef67a561ebb7bbaea671c7139d10699
x-19981231 <hvfyd24685@yahoo.co.jp>
2025-10-07 17:47:48 +0900

    授業における内容追加

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -3,11 +3,15 @@ package raisetech.student.management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class Application {
+
+	private String name = "Enami Kouji";
+	private String age = "37";
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -23,4 +27,21 @@ public class Application {
 		return "こんにちは!今日はいい天気ですね";
 	}
 
+	@GetMapping("/studentInfo")
+	public String getstudentInfo(){
+		return name + "" + age + "歳";
+	}
+	@GetMapping("/age")
+	public String getAge(){
+		return age;
+	}
+	@PostMapping("/studentInfo")
+	public void setstudentInfo(String name,String age){
+		this.name = name;
+		this.age = age;
+	}
+	@PostMapping("studentName")
+public void updateStudentName(String name){
+		this.name = name;
+	}
 }
