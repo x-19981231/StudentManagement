cb0d66007bf901c2af641c293f6c6f9e67663770
x-19981231 <hvfyd24685@yahoo.co.jp>
2026-01-29 15:48:54 +0900

    スタンダード第15回演習課題

--- src/main/java/raisetech/student/management/controller/StudentController.java
+++ src/main/java/raisetech/student/management/controller/StudentController.java
@@ -4,6 +4,9 @@ package raisetech.student.management.controller;
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import raisetech.student.management.controller.converter.StudentConverter;
 import raisetech.student.management.data.Student;
 import raisetech.student.management.domain.StudentDetail;
@@ -11,7 +14,6 @@ import raisetech.student.management.service.StudentService;
 import raisetech.student.management.data.StudentCourse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class StudentController {
@@ -40,4 +42,21 @@ public class StudentController {
     return service.searchStudentsCourseList();
   }
 
+  @GetMapping("/newStudent")
+  public String newStudent(Model model){
+    model.addAttribute("studentDetail", new StudentDetail());
+    return "registerStudent";
+  }
+
+  @PostMapping("/registerStudent")
+  public  String registerStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result){
+    if (result.hasErrors()){
+      return  "registerStudent";
+    }
+
+    service.registerStudent(studentDetail.getStudent());
+    return "redirect:/studentList";
+  }
+
 }
+
